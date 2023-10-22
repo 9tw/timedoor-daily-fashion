cb5a02094e190e5ae9b0ad7a06476e43c1d2e05a
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-10-22 13:01:33 +0900

    add category

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-import { StyleSheet } from 'react-native';
-import { imageSlider } from '../../data/Data';
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import { imageSlider, categoryList } from '../../data/Data';
 import { SliderBox } from 'react-native-image-slider-box';
 
 const HomeScreen = () => {
@@ -13,6 +12,32 @@ const HomeScreen = () => {
                 circleLoop={true}
                 sliderBoxHeight={250}
             />
+            <View style={styles.titleContainer}>
+                <Text style={styles.text}>
+                    Categories
+                </Text>
+                <FlatList
+                    data={categoryList}
+                    key={3}
+                    numColumns={3}
+                    keyExtractor={(item) => item.id}
+                    contentContainerStyle={styles.flatListContainer}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item }) => {
+                        return (
+                            <TouchableOpacity
+                                style={styles.button}
+                            >
+                                <Image
+                                    source={{ uri: item.icon }}
+                                    style={styles.icon}
+                                />
+                                <Text style={styles.itemName} >{item.name}</Text>
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+            </View>
         </View>
     )
 }
@@ -21,6 +46,36 @@ const styles = StyleSheet.create({
     mainContainer: {
         backgroundColor: 'white',
         flex: 1
+    },
+    titleContainer: {
+        marginTop: 16,
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    flatListContainer: {
+        padding: 8
+    },
+    button: {
+        // flex: 1,
+        margin: 8,
+        borderWidth: 1,
+        borderColor: '#7CAF58',
+        borderRadius: 10,
+        height: 130,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    icon: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain'
+    },
+    itemName: {
+        color: 'black'
     }
 });
 
