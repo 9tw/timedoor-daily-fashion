7501abcaa7fb6bea51c35fa53969148c06c3a93d
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-10-22 13:54:54 +0900

    add form

--- src/screens/AddProductScreen.js
+++ src/screens/AddProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, TouchableOpacity, Image, ScrollView, StyleSheet } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import { InputComponent } from '../components/InputComponent';
@@ -37,6 +37,10 @@ const AddProductScreen = () => {
             [type]: value
         });
     };
+
+    useEffect(() => {
+        console.log(productData);
+    }, [productData]);
 
     return (
         <View style={styles.mainContainer}>
@@ -83,6 +87,38 @@ const AddProductScreen = () => {
                         type="font-awesome"
                     />
                 </View>
+                <Text style={styles.sellerText}>Seller Contact</Text>
+                <InputComponent
+                    placeholder='Whastapp number (ex : +4498739230)'
+                    value={productData.phoneNumber}
+                    onChangeText={(text) => onInputChange('phoneNumber', text)}
+                    isIcon={true}
+                    name="whatsapp"
+                    type="font-awesome"
+                />
+                <InputComponent
+                    placeholder='Instagram username (ex : timedooracademy)'
+                    value={productData.instagram}
+                    onChangeText={(text) => onInputChange('instagram', text)}
+                    isIcon={true}
+                    name="instagram"
+                    type="font-awesome"
+                />
+                <InputComponent
+                    placeholder='Facebook username (ex : timedooracademy)'
+                    value={productData.facebook}
+                    onChangeText={(text) => onInputChange('facebook', text)}
+                    isIcon={true}
+                    name="facebook-square"
+                    type="font-awesome"
+                />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.saveButton}
+                    >
+                        <Text style={styles.saveText}>SAVE</Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         </View>
     );
@@ -112,6 +148,30 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'flex-end',
     },
+    sellerText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 16,
+        marginLeft: 8,
+        marginBottom: 0,
+        color: 'black'
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 8
+    },
+    saveButton: {
+        marginTop: 16,
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        backgroundColor: 'mistyrose'
+    },
+    saveText: {
+        color: 'black'
+    }
 });
 
 export default AddProductScreen
