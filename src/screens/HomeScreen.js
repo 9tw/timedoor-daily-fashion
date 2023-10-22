5a9654c01c313e559a287b42833b9a9eb2ac8712
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-10-22 12:22:02 +0900

    image slider

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native';
+import { imageSlider } from '../../data/Data';
+import { SliderBox } from 'react-native-image-slider-box';
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    return (
+        <View style={styles.mainContainer}>
+            <SliderBox
+                images={imageSlider}
+                autoplay={true}
+                circleLoop={true}
+                sliderBoxHeight={250}
+            />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: 'white',
+        flex: 1
+    }
+});
 
 export default HomeScreen
