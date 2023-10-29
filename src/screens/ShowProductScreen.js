e9ad03ef4207fe10a46492faa6f85750d873bb20
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-10-30 00:18:15 +0900

    show contact on product

--- src/screens/ShowProductScreen.js
+++ src/screens/ShowProductScreen.js
@@ -5,16 +5,32 @@ import {
     View, Text, FlatList,
     Image, TouchableOpacity, StyleSheet
 } from 'react-native';
+import { MediaComponent } from '../components/MediaComponent';
 
 const ShowProductScreen = (props) => {
     const { navigation } = props;
     const [data, setData] = useState([]);
     const { route } = props;
     const category = route.params.categoryId;
+    const [isBuy, setIsBuy] = useState(false);
+    const [contact, setContact] = useState({
+        phoneNumber: '',
+        instagram: '',
+        facebook: ''
+    });
 
     const collectData = () => {
         const allData = realm.objects('Product').filtered(`category = ${category}`);
         setData(allData);
+    };
+
+    const buyProduct = (whatsapp, instagramId, facebookId) => {
+        setContact({
+            phoneNumber: whatsapp,
+            instagram: instagramId,
+            facebook: facebookId
+        });
+        setIsBuy(true);
     };
 
     useEffect(() => {
@@ -52,7 +68,9 @@ const ShowProductScreen = (props) => {
                                     </Text>
                                 </View>
                             </View>
-                            <TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => buyProduct(item.phoneNumber, item.instagram, item.facebook)}
+                            >
                                 <Icon
                                     name="shoppingcart"
                                     type="antdesign"
@@ -68,6 +86,58 @@ const ShowProductScreen = (props) => {
                     </View>
                 }
             />
+            {
+                isBuy ?
+                    <View style={styles.modalContainer}>
+                        <View style={styles.box}>
+                            <TouchableOpacity
+                                style={styles.cancel}
+                                onPress={() => setIsBuy(false)}
+                            >
+                                <Icon
+                                    name="close"
+                                    type="antdesign"
+                                    size={18}
+                                />
+                            </TouchableOpacity>
+                            <Text style={[
+                                styles.sellerText,
+                                styles.title
+                            ]}>
+                                Contact the seller through this media :
+                            </Text>
+                            {
+                                contact.phoneNumber !== '' ?
+                                    <MediaComponent
+                                        source={require('../../assets/images/whatsapp.png')}
+                                        value={contact.phoneNumber}
+                                    />
+                                    :
+                                    null
+                            }
+                            {
+                                contact.instagram !== '' ?
+                                    <MediaComponent
+                                        source={require('../../assets/images/instagram.png')}
+                                        value={contact.instagram}
+                                    />
+                                    :
+                                    null
+                            }
+                            {
+                                contact.facebook !== '' ?
+                                    <MediaComponent
+                                        source={require('../../assets/images/facebook.png')}
+                                        value={contact.facebook}
+                                    />
+                                    :
+                                    null
+                            }
+                        </View>
+                    </View>
+                    :
+                    null
+            }
         </View>
     )
 }
@@ -111,6 +181,32 @@ const styles = StyleSheet.create({
     text: {
         color: 'black',
         fontSize: 16
+    },
+    modalContainer: {
+        backgroundColor: 'rgba(255,255,255,0.9)',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    box: {
+        width: '80%',
+        backgroundColor: 'white',
+        padding: 16,
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    cancel: {
+        padding: 8,
+        position: 'absolute',
+        right: 8,
+        top: 8
+    },
+    sellerText: {
+        marginBottom: 8,
+        marginTop: 32
     }
 });
 
